bead2ac88770f8626e5b3913898dbc191c3cae83
DebasisDa <debasisdasnospdii@gmail.com>
2024-05-29 12:19:35 +0530

    componenet change without ofscreencanvas

--- Tenserflow_Js/ai-video-backgrounds-mediapipe/src/App.js
+++ Tenserflow_Js/ai-video-backgrounds-mediapipe/src/App.js
@@ -5,15 +5,17 @@ import { BlurBackground } from './Components/BlurBackground';
 import { ImageBackground } from './Components/ImageBackground';
 import { BlurBackgroundWebWorker } from './Components/webWorker/BlurBackgroundWebWorker';
 import { BlurWithOffScreenCanvas } from './Components/BlurWithOffScreenCanvas';
+import { BlurWithOffScreenWithOutCanvas } from './Components/BlurWithOffScreenWithOutCanvas';
 
 function App() {
   return (
     // <NoEffect/>
-    <ColorBackground />
+    // <ColorBackground />
     // <BlurBackground/>
     //  <ImageBackground/>
     // <BlurBackgroundWebWorker/>
     // <BlurWithOffScreenCanvas/>
+    <BlurWithOffScreenWithOutCanvas/>
   );
 }
 
--- Tenserflow_Js/ai-video-backgrounds-mediapipe/src/Components/BlurWithOffScreenWithOutCanvas.js
+++ Tenserflow_Js/ai-video-backgrounds-mediapipe/src/Components/BlurWithOffScreenWithOutCanvas.js
@@ -4,13 +4,14 @@ import "../App.css";
 import backImage from './2.jpg';
 import { Director, Publish } from "@millicast/sdk";
 
-function BlurWithOffScreenCanvas() {
-    const inputVideoRef = useRef();
-    const canvasRef = useRef();
-    const contextRef = useRef();
-    const offscreenCanvas = useRef(null);
-    const [resetBut, setResetBut] = useState('yesssss');
+function BlurWithOffScreenWithOutCanvas() {
+    const inputVideoRef = useRef(null);
+    const canvasRef = useRef(null);
+    const contextRef = useRef(null);
+    //const canvasRef = useRef(null);
     let mediaStreamRef = useRef(null);
+
+    const [resetBut, setResetBut] = useState('yesssss');
 
     const [backgroundState, setBackgroundState] = useState({
         none: true,
@@ -20,13 +21,8 @@ function BlurWithOffScreenCanvas() {
     let imgage;
 
     useEffect(() => {
-        offscreenCanvas.current = canvasRef.current.transferControlToOffscreen();
-    }, [resetBut]);
-
-    useEffect(() => {
-        //offscreenCanvas.current = canvasRef.current.transferControlToOffscreen();
         imgage = document.getElementById("background");
-        contextRef.current = offscreenCanvas.current.getContext("2d");
+        contextRef.current = canvasRef.current.getContext("2d");
         const constraints = {
             video: true,
             Audio: true
@@ -60,26 +56,29 @@ function BlurWithOffScreenCanvas() {
         };
     }, [backgroundState.none, backgroundState.blur, backgroundState.img, resetBut]);
 
+  
+
     const onResults = (results) => {
+        if (!contextRef.current || !canvasRef.current) return;
         contextRef.current.save();
         contextRef.current.clearRect(
             0,
             0,
-            offscreenCanvas.current.width,
-            offscreenCanvas.current.height
+            canvasRef.current.width,
+            canvasRef.current.height
         );
         contextRef.current.drawImage(
             results.segmentationMask,
             0,
             0,
-            offscreenCanvas.current.width,
-            offscreenCanvas.current.height
+            canvasRef.current.width,
+            canvasRef.current.height
         );
 
         //Will show exact video without background effect
         contextRef.current.globalCompositeOperation = 'source-in';
-        contextRef.current.drawImage(results.image, 0, 0, offscreenCanvas.current.width,
-            offscreenCanvas.current.height);
+        contextRef.current.drawImage(results.image, 0, 0, canvasRef.current.width,
+            canvasRef.current.height);
 
         //Person (no change)
         // contextRef.current.globalCompositeOperation = "destination-atop";
@@ -88,8 +87,8 @@ function BlurWithOffScreenCanvas() {
         //     results.image,
         //     0,
         //     0,
-        //     offscreenCanvas.current.width,
-        //     offscreenCanvas.current.height
+        //     canvasRef.current.width,
+        //     canvasRef.current.height
         // );
         if (backgroundState.blur) {
             contextRef.current.globalCompositeOperation = "destination-atop";
@@ -98,8 +97,8 @@ function BlurWithOffScreenCanvas() {
                 results.image,
                 0,
                 0,
-                offscreenCanvas.current.width,
-                offscreenCanvas.current.height
+                canvasRef.current.width,
+                canvasRef.current.height
             );
         } else if (backgroundState.none) {
             contextRef.current.globalCompositeOperation = "destination-atop";
@@ -107,8 +106,8 @@ function BlurWithOffScreenCanvas() {
                 results.image,
                 0,
                 0,
-                offscreenCanvas.current.width,
-                offscreenCanvas.current.height
+                canvasRef.current.width,
+                canvasRef.current.height
             );
         } else {
             contextRef.current.globalCompositeOperation = "destination-atop";
@@ -116,21 +115,35 @@ function BlurWithOffScreenCanvas() {
                 imgage,
                 0,
                 0,
-                offscreenCanvas.current.width,
-                offscreenCanvas.current.height
+                canvasRef.current.width,
+                canvasRef.current.height
             );
         }
         contextRef.current.restore();
-        mediaStreamRef.current = offscreenCanvas.current.captureStream(30);
+        mediaStreamRef.current = canvasRef.current.captureStream(30);
     };
 
 
-    const reset = () => {
+    const reset = async () => {
         console.log("calling change /......");
-        if(resetBut == 'yesssss')
-          setResetBut('no');
-        else 
+        if (resetBut == 'yesssss')
+            setResetBut('no');
+        else
             setResetBut('yesssss');
+
+        setTimeout(async () => {  
+            const broadCastOptions = {
+                mediaStream: mediaStreamRef.current,
+                sourceId: "Debasis",
+            };
+    
+            const tokenGenerator = () => Director.getPublisher({
+                token: "73a3aca60f07ef0822826c9934198a3d778f45f0b3b6520c31470ee2ae481165",
+                streamName: "simple",
+            });
+            const publisher = new Publish("simple", tokenGenerator);
+            await publisher.connect(broadCastOptions);
+        }, 1000);
     };
     console.log("calling change 222/......");
 
@@ -164,16 +177,16 @@ function BlurWithOffScreenCanvas() {
         <div>
             {resetBut == 'yesssss' &&
                 <div className="App">
-                    <video autoPlay ref={inputVideoRef} style={{ position: "absolute", display:"none"  }} width={800} height={500} />
+                    <video autoPlay ref={inputVideoRef} style={{ position: "absolute", display: "none" }} width={800} height={500} />
                     <canvas ref={canvasRef} width={800} height={500} style={{ position: "absolute" }} />
                 </div>
             }
-            
+
             {resetBut == 'no' &&
-            <div className="App">
-                <video autoPlay ref={inputVideoRef} style={{ position: "absolute", display:"none" }} width={100} height={100} />
-                <canvas ref={canvasRef} width={100} height={100} style={{ position: "absolute" }} />
-            </div>
+                <div className="App">
+                    <video autoPlay ref={inputVideoRef} style={{ position: "absolute", display: "none" }} width={100} height={100} />
+                    <canvas ref={canvasRef} width={100} height={100} style={{ position: "absolute" }} />
+                </div>
             }
             <button type="reset" value="Reset" onClick={reset}>Reset</button>
             <button type="reset" value="Reset" onClick={blur}>blur</button>
@@ -191,4 +204,4 @@ function BlurWithOffScreenCanvas() {
     );
 }
 
-export { BlurWithOffScreenCanvas };
+export { BlurWithOffScreenWithOutCanvas };
